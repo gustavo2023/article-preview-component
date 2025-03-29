dd6f181ddc6d682d2869dc5c5f5dcc918f6e298b
Gustavo Gutierrez <gustavo2023@gmail.com>
2025-03-28 21:39:31 -0400

    refactor share button functionality to support multiple buttons and improve tooltip behavior

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const main = document.querySelector("main");
 const authorInfoContainer = document.querySelector(".author-info-container");
 const authorInfo = document.querySelector(".author-info");
 const socialLinksBox = document.querySelector(".socials-links-box");
-const shareBtn = document.querySelector(".share-btn");
+const shareBtns = document.querySelectorAll(".share-btn");
 
 socialLinksBox.style.display = "none";
 
 const positionTooltip = () => {
-  const btnRect = shareBtn.getBoundingClientRect();
+  const btnRect = shareBtns[0].getBoundingClientRect();
   const mainRect = main.getBoundingClientRect();
 
   socialLinksBox.style.top = `${
@@ -20,16 +20,32 @@ const positionTooltip = () => {
 };
 
 const closeTooltip = (e) => {
-  if (!socialLinksBox.contains(e.target) && !shareBtn.contains(e.target)) {
+  if (!socialLinksBox.contains(e.target) && !shareBtns.contains(e.target)) {
     socialLinksBox.style.display = "none";
   }
 };
 
-shareBtn.addEventListener("click", () => {
+const displaySocialLinks = () => {
+  authorInfo.style.display = "none";
+  authorInfoContainer.style.backgroundColor = "#48556a";
+  socialLinksBox.style.display = "flex";
+};
+
+shareBtns[0].addEventListener("click", () => {
   if (socialLinksBox.style.display === "none") {
     positionTooltip();
   } else {
     socialLinksBox.style.display = "none";
+  }
+});
+
+shareBtns[1].addEventListener("click", () => {
+  if (socialLinksBox.style.display === "none") {
+    displaySocialLinks();
+  } else {
+    socialLinksBox.style.display = "none";
+    authorInfo.style.display = "flex";
+    authorInfoContainer.style.backgroundColor = "#fff";
   }
 });
 
