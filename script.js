1b6b25c5784b183866a02e819899ae990cea0371
Gustavo Gutierrez <gustavo2023@gmail.com>
2025-03-28 22:21:20 -0400

    refactor share button to use SVG icon and improve styling for better visibility

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const authorInfoContainer = document.querySelector(".author-info-container");
 const authorInfo = document.querySelector(".author-info");
 const socialLinksBox = document.querySelector(".socials-links-box");
 const shareBtns = document.querySelectorAll(".share-btn");
+const svgPath = document.querySelector(".share-icon-path");
 
 socialLinksBox.style.display = "none";
 
@@ -28,8 +29,10 @@ const closeTooltip = (e) => {
 const displaySocialLinks = () => {
   authorInfo.style.display = "none";
   authorInfoContainer.style.backgroundColor = "#48556a";
-  authorInfoContainer.style.padding = "0.874rem 2rem";
+  authorInfoContainer.style.padding = "0.874rem 2rem 0.875rem 1rem";
   socialLinksBox.style.display = "flex";
+  shareBtns[1].style.backgroundColor = "#6E8098";
+  svgPath.style.fill = "#fff";
 };
 
 shareBtns[0].addEventListener("click", () => {
@@ -48,6 +51,8 @@ shareBtns[1].addEventListener("click", () => {
     authorInfo.style.display = "flex";
     authorInfoContainer.style.backgroundColor = "#fff";
     authorInfoContainer.style.padding = "0 2rem 1.25rem 2rem";
+    shareBtns[1].style.backgroundColor = "#ecf2f8";
+    svgPath.style.fill = "#6E8098";
   }
 });
 
