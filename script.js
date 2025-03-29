df60d1cedf7ab5d35d89775f755f70c448841ab5
Gustavo Gutierrez <gustavo2023@gmail.com>
2025-03-29 00:12:14 -0400

    refactor share button classes and event listeners for improved clarity and functionality

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ document.addEventListener("DOMContentLoaded", () => {
   const authorInfoContainer = document.querySelector(".author-info-container");
   const authorInfo = document.querySelector(".author-info");
   const socialLinksBox = document.querySelector(".socials-links-box");
-  const shareBtns = document.querySelectorAll(".share-btn");
+  const shareBtnDesktop = document.querySelector(".share-btn.desktop");
+  const shareBtnMobile = document.querySelector(".share-btn.mobile");
   const svgPath = document.querySelector(".share-icon-path");
 
   socialLinksBox.style.display = "none";
 
   const positionTooltip = () => {
-    const btnRect = shareBtns[0].getBoundingClientRect();
+    const btnRect = shareBtnDesktop.getBoundingClientRect();
     const mainRect = main.getBoundingClientRect();
 
     socialLinksBox.style.top = `${
@@ -21,26 +22,16 @@ document.addEventListener("DOMContentLoaded", () => {
     socialLinksBox.style.display = "flex";
   };
 
-  const closeTooltip = (e) => {
-    const isClickInsideButton = Array.from(shareBtns).some((btn) =>
-      btn.contains(e.target)
-    );
-
-    if (!socialLinksBox.contains(e.target) && !isClickInsideButton) {
-      socialLinksBox.style.display = "none";
-    }
-  };
-
   const displaySocialLinks = () => {
     authorInfo.style.display = "none";
     authorInfoContainer.style.backgroundColor = "#48556a";
     authorInfoContainer.style.padding = "0.874rem 2rem 0.875rem 1rem";
     socialLinksBox.style.display = "flex";
-    shareBtns[1].style.backgroundColor = "#6E8098";
+    shareBtnMobile.style.backgroundColor = "#6E8098";
     svgPath.style.fill = "#fff";
   };
 
-  shareBtns[0].addEventListener("click", () => {
+  shareBtnDesktop.addEventListener("click", () => {
     if (socialLinksBox.style.display === "none") {
       positionTooltip();
     } else {
@@ -48,7 +39,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  shareBtns[1].addEventListener("click", () => {
+  shareBtnMobile.addEventListener("click", () => {
     if (socialLinksBox.style.display === "none") {
       displaySocialLinks();
     } else {
@@ -56,10 +47,8 @@ document.addEventListener("DOMContentLoaded", () => {
       authorInfo.style.display = "flex";
       authorInfoContainer.style.backgroundColor = "#fff";
       authorInfoContainer.style.padding = "0 2rem 1.25rem 2rem";
-      shareBtns[1].style.backgroundColor = "#ecf2f8";
+      shareBtnMobile.style.backgroundColor = "#ecf2f8";
       svgPath.style.fill = "#6E8098";
     }
   });
-
-  document.addEventListener("click", closeTooltip);
 });
